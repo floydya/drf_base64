acc891ecb622a4692776c54051826c1c2cbdb6ac
Emmanuelle Delescolle <emma@levit.be>
2018-04-13 08:07:29 +0200

    :documentation: :bug: fix django versions in setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',  # example license
         'Operating System :: OS Independent',
