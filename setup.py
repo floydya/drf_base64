d10f72041743d0f4fda510f83cf80e0671bb1d7a
Emmanuelle Delescolle <emma@levit.be>
2019-06-07 16:24:36 +0200

    :bookmark: V1.0.1

--- setup.py
+++ setup.py
@@ -15,12 +15,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='drf_base64',
-    version='1.0',
+    version='1.0.1',
     packages=['drf_base64'],
     include_package_data=True,
     license='MIT License',  # example license
     description='DRF serializers to handle base64-encoded files',
     long_description=README,
+    long_description_content_type="text/markdown",
     url='https://bitbucket.org/levit_scs/drf_base64',
     author='LevIT SCS',
     author_email='info@levit.be',
@@ -31,11 +32,16 @@ setup(
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',  # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
